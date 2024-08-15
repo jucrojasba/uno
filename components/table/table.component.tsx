2729215d8e03c74348739f2a2e8b11784c6afc97
sebas1913 <sebasosornicastrillon@gmail.com>
2024-08-15 08:13:00 -0500

    Sebastián Osorno: Se culminan los estilos de la tabla

--- components/table/table.component.tsx
+++ components/table/table.component.tsx
@@ -5,83 +5,84 @@ import { getProducts } from "@/utilities/get-products.utility";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-//Implementation of Styled Component
+// Implementation of Styled Component
 
 const Main = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 80%;
   margin: auto;
-`
+`;
+
 const Table = styled.table`
-  background-color: lightblue;
-`
+  text-align: center;
+  width: 80%;
+  border-collapse: separate;
+  border-spacing: 0; 
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
 const Thead = styled.thead`
-  border-radius: 3px;
-`
-const Tbody = styled.tbody`
-  background-color: #454545;
-  border-radius: 3px;
-`
+  background-color: #000;
+  font-size: 22px;
+`;
+
+const Tbody = styled.tbody``;
 
 const Tr = styled.tr`
-  padding: 10px;
-  border-radius: 3px;
-`
+`;
+
 const Th = styled.th`
   padding: 10px;
-  border-radius: 3px;
   color: #fff;
-`
+`;
+
 const Td = styled.td`
   padding: 10px;
-  border-radius: 3px;
-  color: #fff;
-`
+  color: #000;
+  background: #e2c99f;
+  border-bottom: 3px solid #fff;
+`;
 
 export default function TableComponent() {
+  const [products, setProducts] = useState<Product[]>([]);
 
-    const [products, setProducts] = useState<Product[]>([])
-
-    useEffect(()=> {
-      localStorage.setItem('products', JSON.stringify([
-        {
-          id: 1,
-          title: 'Producto 1',
-          description: 'Descripción del producto 1',
-          price: 100,
-          image: '/next.svg'
-        },
-        {
-          id: 2,
-          title: 'Producto 2',
-          description: 'Descripción del producto 2',
-          price: 200,
-          image: '/vercel.svg'
-        },
-        {
-          id: 3,
-          title: 'Producto 3',
-          description: 'Descripción del producto 3',
-          price: 300,
-          image: '/vercel.svg'
-        }
-      ]));
-      
-      
-
-      const productsList = getProducts()
-      if(productsList){
-        setProducts(productsList)
+  useEffect(() => {
+    localStorage.setItem('products', JSON.stringify([
+      {
+        id: 1,
+        title: 'Producto 1',
+        description: 'Descripción del producto 1',
+        price: 100,
+        image: '/next.svg'
+      },
+      {
+        id: 2,
+        title: 'Producto 2',
+        description: 'Descripción del producto 2',
+        price: 200,
+        image: '/vercel.svg'
+      },
+      {
+        id: 3,
+        title: 'Producto 3',
+        description: 'Descripción del producto 3',
+        price: 300,
+        image: '/vercel.svg'
       }
-    }, [])
+    ]));
 
+    const productsList = getProducts();
+    if (productsList) {
+      setProducts(productsList);
+    }
+  }, []);
 
-    return (
-      <Main>
-       <Table>
+  return (
+    <Main>
+      <Table>
         <Thead>
           <Tr>
             <Th>ID</Th>
@@ -94,21 +95,19 @@ export default function TableComponent() {
         <Tbody>
           {products.map((product: Product) => {
             return (
-              <Tr>
+              <Tr key={product.id}>
                 <Td>{product.id}</Td>
                 <Td>{product.title}</Td>
                 <Td>{product.price}</Td>
                 <Td>{product.description}</Td>
                 <Td>
-                  <Image src={product.image} alt="" width={100} height={100}/>
+                  <Image src={product.image} alt={product.title} width={100} height={100} />
                 </Td>
               </Tr>
-            )
+            );
           })}
-    
         </Tbody>
       </Table>
-      </Main>
-    );
-  }
-
+    </Main>
+  );
+}
