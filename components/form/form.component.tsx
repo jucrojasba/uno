6942f06a7a927f2d977a9668929fff02e912f130
CristhianDavid0207 <emmacris04@gmail.com>
2024-08-15 08:51:59 -0500

    Edwin Carmona: implementation of message alerts and styled components in the form component

--- components/form/form.component.tsx
+++ components/form/form.component.tsx
@@ -6,33 +6,46 @@ import Button from "@/components/form/button/button.component"
 import styled from "styled-components"
 import { setProduct } from "@/utilities/set-product.utility"
 import { Product } from "@/models/product.model"
+import Swal from "sweetalert2"
 
-const Container = styled.div`
-    max-width: 600px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #f5f5f5;
-    border-radius: 5px;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
-    h2 {
-        margin-bottom: 20px;
-    }
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-    }
+const FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+        
     button {
         width: 100%;
+        padding: 10px;
+        background-color: #333;
+        color: #fff;
+        border: none;
+        border-radius: 8px;
+        cursor: pointer;
     }
-    input, textarea, button {
-        width: 100%;
+
+    button:hover {
+        opacity: .7;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out;
     }
-    input:focus, button:focus {
+
+    input{
+        padding: 10px;
+        border-radius: 8px;
+    }
+
+    textarea {
+        height: 100px;
+        resize: none;
+        padding: 10px;
+        border-radius: 8px;
+    }
+    
+    input:focus, textarea:focus, button:focus {
         outline: none;
         border-color: black;
     }
-`
+    `
 
 function Form() {
     const newProduct: Product = {
@@ -62,43 +75,54 @@ function Form() {
 
         setProduct(productToSave);
 
+        Swal.fire({
+            title: 'Producto Guardado',
+            text: 'El producto se ha guardado correctamente.',
+            icon:'success',
+            confirmButtonText: 'Aceptar'
+        })
+
         setProductState(newProduct);
     };
 
+    const resetInput = () => {
+        setProductState(newProduct);
+    }
+
     return (
-        <Container>
-            <h2>Crear Producto</h2>
-            <form onSubmit={handleSubmit}>
-                <Input
-                    type="text"
-                    placeholder="Título"
-                    id="title"
-                    value={product.title}
-                    onChange={handleChange}
-                />
-                <Textarea
-                    placeholder="Descripción"
-                    id="description"
-                    value={product.description}
-                    onChange={handleChange}
-                />
-                <Input
-                    type="number"
-                    placeholder="Precio"
-                    id="price"
-                    value={String(product.price)}
-                    onChange={handleChange}
-                />
-                <Input
-                    type="url"
-                    placeholder="URL Imagen"
-                    id="image"
-                    value={product.image}
-                    onChange={handleChange}
-                />
-                <Button type="submit" label="Guardar" />
-            </form>
-        </Container>
+        <FormContainer onSubmit={handleSubmit}>
+            <Input
+                type="text"
+                placeholder="Título"
+                id="title"
+                value={product.title}
+                onChange={handleChange}
+                required
+            />
+            <Textarea
+                placeholder="Descripción"
+                id="description"
+                value={product.description}
+                onChange={handleChange}
+            />
+            <Input
+                type="number"
+                placeholder="Precio"
+                id="price"
+                value={String(product.price)}
+                onChange={handleChange}
+                required
+            />
+            <Input
+                type="url"
+                placeholder="URL Imagen"
+                id="image"
+                value={product.image}
+                onChange={handleChange}
+            />
+            <Button type="submit" label="Guardar" />
+            <Button type="button" label="Limpiar" onClick={resetInput} />
+        </FormContainer>
     )
 }
 
