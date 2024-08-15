81a1fae16fcfa1ffae6a4743126d8436b3c0bf5e
CristhianDavid0207 <emmacris04@gmail.com>
2024-08-15 08:54:10 -0500

    Edwin Carmona: label is added in the fields

--- components/form/textarea/textarea.component.tsx
+++ components/form/textarea/textarea.component.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { TextareaProps } from "@/models/textarea.model";
 
 const Textarea: React.FC<TextareaProps> = ({ placeholder, id, value, onChange }) => {
-  return <textarea placeholder={placeholder} id={id} value={value} onChange={onChange} />;
+  return (
+    <>
+      <label htmlFor={id}>{placeholder}</label>
+      <textarea placeholder={placeholder} id={id} value={value} onChange={onChange} />
+    </>
+  );
 };
 
 export default Textarea;
--- models/input.model.tsx
+++ models/input.model.tsx
@@ -4,4 +4,5 @@ export interface InputProps {
     id: string;
     value: string;
     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean;
  }
