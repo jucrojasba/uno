06560b3098373dea0d21bb478e318f1d06feb456
ecc97 <carmonaedwin1123@gmail.com>
2024-08-16 12:05:35 -0500

    Edwin Carmona: Adding styled components to the ui buttons of the form

--- components/UI/buttonCreate/buttonCreate.ui.tsx
+++ components/UI/buttonCreate/buttonCreate.ui.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import { ButtonFormProps } from '@/models/buttonForm.model';
+import { GlobalTheme } from '@/app/GlobalStyling';
+import styled from 'styled-components';
+
+const ButtonCreateStyle = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin-top: 10px;
+  background-color: ${GlobalTheme.pageColors.bgTertiary};
+  color: ${GlobalTheme.pageColors.textPrimary};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-weight: bold;
+
+  &:hover {
+    opacity: .8;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+`
 
 const ButtonCreate: React.FC<ButtonFormProps> = ({ type, label, onClick }) => {
-  return <button type={type} onClick={onClick}>{label}</button>;
+  return <ButtonCreateStyle type={type} onClick={onClick}>{label}</ButtonCreateStyle>;
 };
 
 export default ButtonCreate
